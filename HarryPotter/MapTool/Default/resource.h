d7995bca9e23a7e618e0464c46f806abcd7b5462
jslauren <zzzbjs@gmail.com>
2019-05-13 19:51:16 +0900

    [MapTool] 와꾸 완성! Effect 빼고 ㅠㅠ

--- HarryPotter/MapTool/Default/resource.h
+++ HarryPotter/MapTool/Default/resource.h
@@ -17,6 +17,58 @@
 #define IDD_EFFECTTAB                   323
 #define IDC_TAB1                        1000
 #define IDC_BUTTON2                     1004
+#define IDC_RADIO1                      1006
+#define IDC_RADIO2                      1007
+#define IDC_RADIO3                      1008
+#define IDC_RADIO4                      1009
+#define IDC_EDIT1                       1010
+#define IDC_RADIO5                      1010
+#define IDC_SPIN1                       1011
+#define IDC_RADIO6                      1011
+#define IDC_EDIT2                       1012
+#define IDC_RADIO7                      1012
+#define IDC_SPIN2                       1013
+#define IDC_RADIO8                      1013
+#define IDC_EDIT3                       1014
+#define IDC_SPIN3                       1015
+#define IDC_EDIT4                       1016
+#define IDC_SPIN4                       1017
+#define IDC_BUTTON1                     1018
+#define IDC_EDIT5                       1019
+#define IDC_SPIN5                       1020
+#define IDC_EDIT6                       1021
+#define IDC_SPIN6                       1022
+#define IDC_EDIT7                       1023
+#define IDC_SPIN7                       1024
+#define IDC_EDIT8                       1025
+#define IDC_SPIN8                       1026
+#define IDC_EDIT9                       1027
+#define IDC_SPIN9                       1028
+#define IDC_BUTTON10                    1028
+#define IDC_EDIT10                      1029
+#define IDC_BUTTON11                    1029
+#define IDC_SPIN10                      1030
+#define IDC_BUTTON12                    1030
+#define IDC_TREE1                       1031
+#define IDC_BUTTON3                     1032
+#define IDC_EDIT11                      1032
+#define IDC_BUTTON4                     1033
+#define IDC_SPIN11                      1033
+#define IDC_BUTTON13                    1033
+#define IDC_EDIT12                      1034
+#define IDC_BUTTON14                    1034
+#define IDC_SPIN12                      1035
+#define IDC_EDIT13                      1036
+#define IDC_SPIN13                      1037
+#define IDC_BUTTON5                     1038
+#define IDC_BUTTON6                     1039
+#define IDC_BUTTON7                     1040
+#define IDC_BUTTON8                     1041
+#define IDC_BUTTON9                     1042
+#define IDC_TREE2                       1043
+#define IDC_TREE3                       1044
+#define IDC_TREE4                       1045
+#define IDC_SLIDER1                     1047
 
 // Next default values for new objects
 // 
@@ -24,7 +76,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        325
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1052
 #define _APS_NEXT_SYMED_VALUE           314
 #endif
 #endif
